19bbe0407d7985b75c86fc34c9a832d7785118af
anshgolakiya <anshgolakiya9909@gmail.com>
2026-03-06 15:04:26 +0530

    Delete pr4.sh

--- pr4.sh
+++
@@ -1,9 +0,0 @@
-echo "enter number"
-read num
-echo "Multiplication table of $num"
-echo "============================"
-for i in {1..10}
-do
-	result=$((num * i))
-	echo " $num x $i = $result "
-	done
